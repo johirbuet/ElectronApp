2644506e1c5312c0c399404aa72de8ca8cc255bf
Johirul Islam <johirbuet@gmail.com>
2017-06-18 18:35:25 -0500

    Multiple window

---
+++ index.js
@@ -0,0 +1,19 @@
+var electron = require('electron');
+var BrowserWindow = electron.BrowserWindow;
+var app = electron.app;
+
+
+app.on('ready', function() {
+    var appWindow, infoWindow;
+    appWindow = new BrowserWindow();
+    appWindow.loadURL('http://raybo.org');
+
+    infoWindow = new BrowserWindow({
+        width: 400,
+        height: 300,
+        transparent: true,
+        frame: false
+    });
+
+    infoWindow.loadURL('file://' + __dirname + '/info.html');
+});
